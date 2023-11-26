dd39f97447087de1579ad25c7ef77172d81b968f
countoren <countoren@gmail.com>
2023-11-26 11:33:25 -0500

    rename createVim to createNeovide

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
 {
   description = "Set of function to create a vims dervation setup with my common configuration";
-  outputs = _: { createVim = import ./neovide.nix; };
+  outputs = _: { createNeovide = import ./neovide.nix; };
 }
