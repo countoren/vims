53fa2efc47bf8db77352d975f199491fbf2e532a
countoren <countoren@gmail.com>
2023-11-26 12:51:49 -0500

    add createNvim function

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
   description = "Set of function to create a vims dervation setup with my common configuration";
-  outputs = _: { createNeovide = import ./neovide.nix; };
+  outputs = _: { 
+    createNeovide = import ./neovide.nix; 
+    createNvim = import ./nvim.nix; 
+
+  };
 }
