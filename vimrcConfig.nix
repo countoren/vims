385ff99da60540ea565136ec63ac36367acf0c8b
Oren Rozen <orozen@carlsonsw.com>
2024-06-07 14:43:10 -0400

    changing to the newer lsp plugin (lsp-conifg )
    change nil to nixd

--- vimrcConfig.nix
+++ vimrcConfig.nix
@@ -83,49 +83,42 @@ in
       indentLine
 
       {
-         plugin = LanguageClient-neovim;
-         config = ''
-          let $PATH = $PATH.":${pkgs.nil}/bin"
+        plugin = nvim-lspconfig; 
+  config =
+  ## Global mappings.
+  ''
+    let $PATH = $PATH.":${pkgs.nixd}/bin"
 
-          if executable('nil')
-          autocmd User lsp_setup call lsp#register_server({
-          \ 'name': 'nil',
-          \ 'cmd': {server_info->['nil']},
-          \ 'whitelist': ['nix'],
-          \ })
-          endif
+    lua << EOF
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-          " identLine
-          " Dont conceal (json)
-          let g:indentLine_setConceal = 0 
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-          " Language server key bindings
-          function! LC_maps()
-          if has_key(g:LanguageClient_serverCommands, &filetype)
-          nnoremap <F5> :call LanguageClient_contextMenu()<CR>
-          nnoremap <silent> K :call LanguageClient#textDocument_hover()<CR>
-          nnoremap <silent> gd :call LanguageClient#textDocument_definition()<CR>
-          nnoremap <silent> gr :call LanguageClient#textDocument_references()<CR>
-          nnoremap <silent> gn :call LanguageClient#textDocument_rename()<CR>
-          nnoremap <leader>f :call LanguageClient_textDocument_codeAction()<CR>
-          nnoremap <leader>k :call LanguageClient#explainErrorAtPoint()<CR>
-          nnoremap <leader>e :call LanguageClient#diagnosticsNext()<CR>
-          nnoremap <leader>E :call LanguageClient#diagnosticsPrevious()<CR>
-          command! Symbols :call LanguageClient_textDocument_documentSymbol()
-          command! Fix :call LanguageClient_textDocument_codeAction()
-          nnoremap <leader>ld :call LanguageClient#textDocument_definition()<CR>
-          nnoremap <leader>lr :call LanguageClient#textDocument_rename()<CR>
-          nnoremap <leader>lt :call LanguageClient#textDocument_typeDefinition()<CR>
-          nnoremap <leader>lx :call LanguageClient#textDocument_references()<CR>
-          nnoremap <leader>la :call LanguageClient_workspace_applyEdit()<CR>
-          nnoremap <leader>lc :call LanguageClient#textDocument_completion()<CR>
-          nnoremap <leader>lh :call LanguageClient#textDocument_hover()<CR>
-          nnoremap <leader>ls :call LanguageClient_textDocument_documentSymbol()<CR>
-          nnoremap <leader>lm :call LanguageClient_contextMenu()<CR>
-          endif
-          endfunction
-          autocmd FileType * call LC_maps()
-          '';
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+      end,
+    })
+      require('lspconfig').nixd.setup{}
+    EOF
+  '';
       }
 
       # Nix 
