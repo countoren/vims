eb221fba835a8565bfd74b3c3aa60b7cc9d30821
Oren Rozen <orozen@carlsonsw.com>
2024-06-07 09:28:48 -0400

    adding shell command

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   description = "Set of function to create a vims dervation setup with my common configuration";
-  outputs = _: { 
+  outputs = { self, nixpkgs }:
+  let system = "x86_64-linux";
+  pkgs = import nixpkgs { inherit system;};
+  in
+
+  { 
     createNeovide = import ./neovide.nix; 
     createNvim = import ./nvim.nix; 
+    packages.x86_64-linux.default = pkgs.writeShellScriptBin "hi" ''echo "Brock is the man!!!"'';
 
   };
 }
