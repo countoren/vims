82b567012ece50a9e827648c7ed4511e9ea1660f
Oren Rozen <orozen@carlsonsw.com>
2024-06-12 18:08:12 -0400

    add nvim-treesitter-textobjects

--- vimrcConfig.nix
+++ vimrcConfig.nix
@@ -100,6 +100,52 @@ in
       }
       #TODO: maybe need setup
       nvim-lint
+      
+
+      #TODO: setup
+      {
+        plugin = nvim-treesitter-textobjects;
+        config = ''
+lua <<EOF
+require'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+  },
+}
+EOF
+        '';
+      }
+
 
 
       {
