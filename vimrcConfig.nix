40c7519868b0feec35b4004b75c2defa7e078f52
Oren Rozen <orozen@carlsonsw.com>
2024-06-22 07:53:43 -0400

    add command to ctrlp to handle correctly subfolder

--- vimrcConfig.nix
+++ vimrcConfig.nix
@@ -77,6 +77,7 @@ in
             let g:ctrlp_working_path_mode=""
             let g:ctrlp_show_hidden = 1
             let g:ctrlp_custom_ignore = 'node_modules'
+            let g:ctrlp_user_command = [ '.git', 'cd %s && git ls-files . -co --exclude-standard', 'find %s -type f' ]
           '';
         }
 
