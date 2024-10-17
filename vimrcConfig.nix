ad9ef91d55d30759ee8991f425b8a8676571b376
Oren Rozen <orozen@carlsonsw.com>
2024-10-17 15:51:43 -0400

    disable automatic closing of folds in nvim-ufo

--- vimrcConfig.nix
+++ vimrcConfig.nix
@@ -264,9 +264,11 @@ in
             vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
             require('ufo').setup({
+                open_fold_hl_timeout = 0,
                 provider_selector = function(bufnr, filetype, buftype)
                     return {'lsp', 'indent'}
                 end
+                close_fold_kinds = {}, 
             })
             EOF
           '';
